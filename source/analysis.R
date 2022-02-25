6e1b528cfe9c966da19cbfb7173b430fa15367b3
GTGSanicFast <info2sonicyao@gmail.com>
2022-02-25 15:53:17 -0800

    intro

---
+++ source/analysis.R
@@ -0,0 +1,46 @@
+trends <- read.csv("../data/incarceration_trends.csv")
+
+library(dplyr)
+
+sum_info <- list()
+
+trends[trends == 0] <- NA
+
+sub_trends <- trends %>%
+  # mutate(black_jail_ratio = black_jail_pop / total_jail_pop,
+  #        white_jail_ratio = white_jail_pop / total_jail_pop,
+  #        bl_pop_ratio = black_pop_15to64 / total_pop_15to64,
+  #        wt_pop_ratio = white_pop_15to64 / total_pop_15to64) %>%
+  select(year, state, county_name, total_pop, total_pop_15to64, aapi_pop_15to64,
+         black_pop_15to64, latinx_pop_15to64, native_pop_15to64,
+         white_pop_15to64, total_jail_pop, white_jail_pop, black_jail_pop,
+         aapi_jail_pop, latinx_jail_pop, native_jail_pop)
+
+
+
+sum_df <- sub_trends %>%
+  filter(year == 2018) %>%
+  summarise(mean_bl_jail_pop_2018 = mean(black_jail_pop, na.rm = TRUE),
+            mean_wt_jail_pop_2018 = mean(white_jail_pop, na.rm = TRUE),
+            mean_aapi_jail_pop_2018 = mean(aapi_jail_pop, na.rm = TRUE),
+            mean_nat_jail_pop_2018 = mean(native_jail_pop, na.rm = TRUE),
+            mean_lat_jail_pop_2018 = mean(latinx_jail_pop, na.rm = TRUE),
+            mean_bl_pop_2018 = mean(black_pop_15to64, na.rm = TRUE),
+            mean_wt_pop_2018 = mean(white_pop_15to64, na.rm = TRUE),
+            mean_aapi_pop_2018 = mean(aapi_pop_15to64, na.rm = TRUE),
+            mean_nat_pop_2018 = mean(native_pop_15to64, na.rm = TRUE),
+            mean_lat_pop_2018 = mean(latinx_pop_15to64, na.rm = TRUE))
+
+# ratio of mean pop to jail pop 15-64 in 2018
+sum_ratio_2018 <- sum_df %>%
+  summarise(bl_jail_ratio = mean_bl_jail_pop_2018 / mean_bl_pop_2018,
+            wt_jail_ratio = mean_wt_jail_pop_2018 / mean_wt_pop_2018,
+            aapi_jail_ratio = mean_aapi_jail_pop_2018 / mean_aapi_pop_2018,
+            nat_jail_ratio = mean_nat_jail_pop_2018 / mean_nat_pop_2018,
+            lat_jail_ratio = mean_lat_jail_pop_2018 / mean_lat_pop_2018)
+  
+sum_info$bl_ratio <- pull(sum_ratio_2018, bl_jail_ratio)
+sum_info$wt_ratio <- pull(sum_ratio_2018, wt_jail_ratio)
+sum_info$aapi_ratio <- pull(sum_ratio_2018, aapi_jail_ratio)
+sum_info$nat_ratio <- pull(sum_ratio_2018, nat_jail_ratio)
+sum_info$lat_ratio <- pull(sum_ratio_2018, lat_jail_ratio)
